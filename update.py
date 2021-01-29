c9c11194d29c3707fb0c3c2133fd595cfc3f2dc4
Ansh13579 <68729523+Ansh13579@users.noreply.github.com>
2021-01-29 10:34:51 +0530

    Add files via upload

---
+++ update.py
@@ -0,0 +1,21 @@
+#do not dare to copy my program.
+import time
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("!        *_*  T-VIRUS 2.0  *_*       !")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print(" Author : T-Dynamos Team | Coder : Ansh Dadwal")
+print(" ")
+time.sleep(1)
+x=input("WRITE YOUR GREAT NAME TO CONTINUE  ==> ")
+a=input("Do You Want To Continue y/n ==> ")
+if a=="y":
+	print(" ")
+	print("It Was Your Wish")
+	print("Checking Your Device..")
+	time.sleep(0.5)
+	print("Runninng Modules")
+	time.sleep(1)
+	print("Crashing Your Python And Ram")
+	print("BYE BYE :(",x*100000000)
+else:
+	print("You Are Safe :)")
